c9ebace0225e15f6947d212dbf17a899e1484c93
letzgorats <a01030659322@gmail.com>
2024-11-10 17:56:16 +0900

    [fix] kakao_map_js_code_fix

--- team_intro.py
+++ team_intro.py
@@ -69,23 +69,21 @@ def show_team_intro():
         <div id="map" style="width:100%;height:500px;"></div>
         <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={KAKAO_JS_API_KEY}&autoload=false&libraries=services"></script>
         <script>
-            document.addEventListener("DOMContentLoaded", function() {{
-                kakao.maps.load(function() {{
-                    var mapContainer = document.getElementById('map'), 
-                        mapOption = {{
-                            center: new kakao.maps.LatLng({latitude}, {longitude}), 
-                            level: 3
-                        }};
-                    var map = new kakao.maps.Map(mapContainer, mapOption);
-                    var marker = new kakao.maps.Marker({{
-                        map: map,
-                        position: new kakao.maps.LatLng({latitude}, {longitude})
-                    }});
-                    var infowindow = new kakao.maps.InfoWindow({{
-                        content: '<div style="width:150px;text-align:center;padding:6px 0;">부천 클리어 풋살장<br>{bucheon_clear_count}회</div>'
-                    }});
-                    infowindow.open(map, marker);
+            kakao.maps.load(function() {{
+                var mapContainer = document.getElementById('map'), 
+                    mapOption = {{
+                        center: new kakao.maps.LatLng({latitude}, {longitude}), 
+                        level: 3
+                    }};
+                var map = new kakao.maps.Map(mapContainer, mapOption);
+                var marker = new kakao.maps.Marker({{
+                    map: map,
+                    position: new kakao.maps.LatLng({latitude}, {longitude})
                 }});
+                var infowindow = new kakao.maps.InfoWindow({{
+                    content: '<div style="width:150px;text-align:center;padding:6px 0;">부천 클리어 풋살장<br>{bucheon_clear_count}회</div>'
+                }});
+                infowindow.open(map, marker);
             }});
         </script>
     """
